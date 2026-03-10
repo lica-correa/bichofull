eaed2408781114791f676a659aaae95c077ce533
Liandra Correa <batistaliandra2@gmail.com>
2026-03-10 16:11:10 -0400

    Adicionando contrato inicial da API

--- README.md
+++ README.md
@@ -8,3 +8,81 @@ Sistema full stack para simulação do Jogo do Bicho.
 - MySQL
 - React
 - JWT
+
+
+## Contrato inicial da API
+Base da API:
+
+http://localhost:8080/api
+
+### Usuários
+
+Criar usuários
+
+POST /api/usuarios
+
+Exemplo de JSON:
+
+{
+    "nome": "Liandra"
+    "email": "liandra@email.com"
+    "senha": "123456"
+}
+
+Listar usuários
+
+GET /api/usuarios
+
+Buscar usuário
+
+GET /api/usuarios/{id}
+
+Atualizar usuário
+
+PUT /api/usuarios/{id}
+
+Deletar usuário
+
+DELETE /api/usuarios/{id}
+
+### Animais
+
+Listar animais
+
+GET /api/animais
+
+Buscar animal
+
+GET /api/animais/{id}
+
+### Apostas
+
+Criar aposta
+
+POST /api/apostas
+
+Exemplo JSON:
+
+{
+  "usuarioId": 1,
+  "animalId": 5,
+  "valor": 10
+}
+
+Listar apostas
+
+GET /api/apostas
+
+Histórico de apostas de um usuário
+
+GET /api/usuarios/{id}/apostas
+
+### Resultados
+
+Resultado do dia
+
+GET /api/resultados
+
+Criar resultado
+
+POST /api/resultados
---
+++ backend/src/main/java/com/bichofull/backend/entity/Usuario.java
@@ -0,0 +1,71 @@
+package com.bichofull.backend.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios") 
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    private String senha;
+
+    private Double saldo;
+
+    public Usuario(){
+    }
+
+    public Usuario(Long id, String nome, String email, String senha, Double saldo) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.saldo = saldo;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+}
